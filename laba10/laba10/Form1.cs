c4d4382840fec70464e5940cd00332dd2b4978d4
averagge <aleksejporfirev15@gmail.com>
2023-04-03 23:24:49 +0300

    Изменил код формы: добавил методы LoadData(), Separator()

--- laba10/laba10/Form1.cs
+++ laba10/laba10/Form1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Contexts;
 using System.Text;
@@ -15,6 +17,75 @@ namespace laba10
     {
         private Context context;
         public int count;
+
+        public static void LoadData()
+        {
+            if (form1.openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            path = form1.openFileDialog1.FileName;
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                Separator(sr);
+                sr.Close();
+            }
+        }
+        private static void Separator(StreamReader streamReader)
+        {
+            int CurrentFilePosition = 0;
+            int TempMultiplicationResult = 0;
+            int LoadedArrayElement = 0;
+            int TempDigitCapacity = 0;
+            while (streamReader.Peek() != -1)
+            {
+                if (streamReader.Peek() == 32)
+                {
+                    char[] vs = new char[2 * CurrentFilePosition];
+                    streamReader.Read(vs, CurrentFilePosition, 1);
+                    CurrentFilePosition++;
+                }
+                else if (streamReader.Peek() >= 48 && streamReader.Peek() <= 57)
+                {
+                    do
+                    {
+                        if (streamReader.Peek() == -1)
+                        {
+                            break;
+                        }
+                        streamReader.Read(listChar, CurrentFilePosition, 1);
+                        int.TryParse(Convert.ToString(listChar[CurrentFilePosition]), out TempMultiplicationResult);
+                        TempMultiplicationResult *= Convert.ToInt32(Math.Pow(10.0, TempDigitCapacity));
+                        LoadedArrayElement += TempMultiplicationResult;
+                        CurrentFilePosition++;
+                        TempDigitCapacity++;
+                    }
+                    while (streamReader.Peek() != 32);
+                    string output = new string(Convert.ToString(LoadedArrayElement).ToCharArray().Reverse().ToArray());
+                    int.TryParse(output, out LoadedArrayElement);
+                    arrayList.Add(Convert.ToString(LoadedArrayElement));
+                    TempDigitCapacity = 0;
+                    TempMultiplicationResult = 0;
+                    LoadedArrayElement = 0;
+                }
+                else
+                {
+                    MessageBox.Show("Некорректный формат загружаемого файла.");
+                    break;
+                }
+            }
+            Context.array = new int[arrayList.Count];
+            for (int k = 0; k < arrayList.Count; k++)
+            {
+                int.TryParse(arrayList[k], out Context.array[k]);
+            }
+            foreach (int j in Context.array)
+            {
+                content += Convert.ToString(j) + " ";
+            }
+            form1.listBox1.Items.Add(content);
+            form1.listBox1.Items.Add("");
+        }
+
+
         public void AddItemsListBox(int first = -1, int second = -1)
         {
             listBox1.Items.Add("");
@@ -65,5 +136,6 @@ namespace laba10
             }
 
         }
+
     }
 }
